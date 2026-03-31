6b667fe7153e91c75abba0c486c04764cf7aba0c
Saqib <saqibiqbal27772@gmail.com>
2026-03-31 20:03:17 +0500

    chore: update build script to include prisma generation and enable trustHost in auth configuration

--- auth.ts
+++ auth.ts
@@ -11,6 +11,7 @@ import TikTok from "next-auth/providers/tiktok"
 import Reddit from "next-auth/providers/reddit"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  trustHost: true,
   adapter: PrismaAdapter(prisma),
   providers: [
     Google,
